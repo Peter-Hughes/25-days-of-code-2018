a71f505110a81708064beffbe411301379e40b93
Peter Hughes <peterhughes343@yahoo.co.uk>
2018-12-04 22:41:27 +0000

    add attempt at solution 3, WIP

--- src/test/kotlin/com/codeadvent/test/solutions/Day 3.kt
+++ src/test/kotlin/com/codeadvent/test/solutions/Day 3.kt
@@ -7,9 +7,18 @@ class Day3 {
 
     @Test
     fun `Day 3 - Part 1`() {
-        val values = fileAsString("/Day3Part1.txt").replace(" ","")
-                .split("\n").map {  it.split("@", ",", ":", "x") }
-        values.forEach(::println)
+        val values = fileAsString("/Day3Part1.txt")
+                .replace(" ", "").split("\n").map { it.split("@", ",", ":", "x") }
+
+        val map = mutableMapOf<Pair<Int, Int>, Char>()
+
+        values.forEach {
+            (0 until it[3].toInt()).forEach { i ->
+                (0 until it[4].toInt()).forEach { j -> mapValues(map, it[1].toInt() + i, it[2].toInt() + j) }
+            }
+        }
+
+        println(map.values.count { it == 'x' })
     }
 
     @Test
@@ -19,3 +28,6 @@ class Day3 {
     }
 }
 
+fun mapValues(map: MutableMap<Pair<Int, Int>, Char>, x: Int, y: Int) {
+    if (map[Pair(x, y)] == 'o') map[Pair(x, y)] = 'x' else map[Pair(x, y)] = 'o'
+}
